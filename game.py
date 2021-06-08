ad3321601ee2924155efa3b8216688a50f48b9d7
davidek0 <ek200302@gmail.com>
2021-06-08 19:00:44 +0200

    small fixes

---
+++ game.py
@@ -0,0 +1,25 @@
+
+
+class game:
+
+    def __init__(self) -> None:
+        pass
+
+    def Start():
+        pass
+
+    
+    def Check_input(word_def) -> bool:
+        
+        if input == word_def:
+            return True
+        else:
+            return False
+    
+
+    def Score(score) -> int:
+
+        if score:
+            return 1
+        else:
+            return 0
